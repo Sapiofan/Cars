6ed2dd8c0cef8e050c6fe85643677325395b2292
Sapiofan <leshalitova@gmail.com>
2023-02-18 16:03:01 +0200

    added jar package

--- src/test/java/com/sapiofan/cars/CarsApplicationTests.java
+++ src/test/java/com/sapiofan/cars/CarsApplicationTests.java
@@ -3,11 +3,11 @@ package com.sapiofan.cars;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CarsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class CarsApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
